8011e587a9ec869376b061fd5ee2f6569a8eee26
Bruce Milligan <brucemilligan@hotmail.com>
2026-03-11 19:56:19 +0000

    Initial Commit

---
+++ DayShape/DayShapeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DayShapeApp.swift
+//  DayShape
+//
+//  Created by Bruce Milligan on 11/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct DayShapeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
